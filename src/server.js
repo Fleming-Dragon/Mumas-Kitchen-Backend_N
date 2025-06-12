c480eda2b78a29961c51870a8f28c016a1c7ab04
Ved Mahajan <mahajanved1234@gmail.com>
2025-06-12 17:11:59 +0530

    gitignore added and  pkg json updated

--- src/server.js
+++ src/server.js
@@ -14,6 +14,13 @@ const app = express();
 // Middleware
 app.use(helmet());
 app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Vite's default port
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 // Connect to MongoDB
@@ -47,15 +54,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-app.get("/", (req, res) => {
-  res.send("Backend is working!");
-});
-
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Vite's default port
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
