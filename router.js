b1eb2ee228bc9da46ba4ccbec586ad23f61d7b24
Ved Mahajan <mahajanved1234@gmail.com>
2025-08-09 13:25:06 +0530

    router deleted

--- router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Define a default route for testing
-router.get('/', (req, res) => {
-  res.send('Welcome to the router!');
-});
-
-module.exports = router;
